c8cff73a0507b9019fa4bc3c0885e4b6bd6b0166
hochitrung8124 <hochitrung08012004@gmail.com>
2025-04-12 14:55:15 +0700

    datafirt

---
+++ Data/Data/VocabularyItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Data
+{
+    public class VocabularyItem
+    {
+        public Guid VocabularyId { get; set; }
+        public Guid UserId { get; set; }
+        public string Word { get; set; }
+        public string Meaning { get; set; }
+        public string Topic { get; set; }
+        public string ImageUrl { get; set; }
+        public string AudioUrl { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public DateOnly UpdateAt { get; set; }
+
+    }
+}
